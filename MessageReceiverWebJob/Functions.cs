8268a44c83aca99f2cedb6290bb9489c35000b7c
Senthuran Sivananthan [MSFT] <sesivan@microsoft.com>
2016-08-08 18:45:00 -0400

    Added Application Insights to track the machine name that received the message for processing

--- MessageReceiverWebJob/Functions.cs
+++ MessageReceiverWebJob/Functions.cs
@@ -4,6 +4,7 @@ using Microsoft.ServiceBus.Messaging;
 using Demo.ServiceBusQueue.WebJobs.SharedLibrary.Model;
 using System.Threading;
 using System;
+using Microsoft.ApplicationInsights;
 
 namespace MessageReceiverWebJob
 {
@@ -15,11 +16,22 @@ namespace MessageReceiverWebJob
         public static void HandleQueueMessage([ServiceBusTrigger("demoqueue")] BrokeredMessage message,
             TextWriter logger)
         {
+            var start = DateTime.Now;
             var msgControl = message.GetBody<MessageControl>();
 
             logger.WriteLine("Machine Name: {0}, Managed Thread ID: {1}", Environment.MachineName, Thread.CurrentThread.ManagedThreadId);
             logger.WriteLine("Queue message received: {0}", message.MessageId);
             logger.WriteLine("Message Reference ID: {0}", msgControl.ReferenceID);
+
+            #region Application Insights
+            var timeDuration = DateTime.Now - start;
+            var tc = new TelemetryClient();
+            tc.TrackRequest("HandleQueueMessage", DateTimeOffset.UtcNow, timeDuration, "200", true);
+
+            // track the machine name that processed the messsage
+            tc.TrackEvent(Environment.MachineName);
+            tc.Flush();  // ensure that the event is flushed to AI
+            #endregion
         }
     }
 }
--- MessageReceiverWebJob/Program.cs
+++ MessageReceiverWebJob/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.WebJobs;
+using System.Configuration;
 
 namespace MessageReceiverWebJob
 {
@@ -6,6 +8,8 @@ namespace MessageReceiverWebJob
     {
         static void Main()
         {
+            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["AzureApplicationInsights.InstrumentationKey"];
+
             JobHostConfiguration config = new JobHostConfiguration();
             config.UseServiceBus();
 
